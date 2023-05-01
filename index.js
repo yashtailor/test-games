848e13479cef96c8bf65a0540af6bb7b25edc6d5
Yash Tailor <yashtailor2000@gmail.com>
2023-05-01 23:56:36 +0530

    added versel.json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-// if not in production use the port 5000
+// if not in production use the port 3000
 const PORT = process.env.PORT || 3000;
 console.log('server started on port:',PORT);
 app.listen(PORT);
